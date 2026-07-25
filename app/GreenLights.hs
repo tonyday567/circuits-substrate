88ff818602e6a05a76851717e169e2fc0772aaea
Tony Day <tonyday567@gmail.com>
2026-07-26 04:52:59 +1000

    Initial substrate integration canary
    - cabal.project depending on all 13 substrate packages plus circuits-meter
    - substrate.cabal with library and substrate-green-lights executable
    - Substrate.hs smoke-test importing one representative module per package
    - Green-lights executable prints ok per package
    - allow-newer: tdigest:base workaround for GHC 9.14
    - .gitignore for dist-newstyle/

---
+++ app/GreenLights.hs
@@ -0,0 +1,6 @@
+module Main where
+
+import Substrate (greenLights)
+
+main :: IO ()
+main = greenLights
---
+++ src/Substrate.hs
@@ -0,0 +1,51 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
+
+-- | Integration smoke-test for the circuits substrate.
+--
+-- Importing one representative module from each substrate package proves that
+-- the whole set compiles and links together.
+module Substrate (greenLights) where
+
+import Data.Functor.Identity (Identity)
+import Data.Proxy (Proxy (..))
+import Harpie.Array (Array, array)
+import Harpie.NumHask ()
+import NumHask.Diff qualified as NHD
+import NumHask.Prelude (one)
+import NumHask.Space (Point (..))
+import Process.Stats (ma)
+import Circuit.AD qualified as CAD
+import Circuit.Mat (Mat)
+import Circuit.Parser (Parser, These, char, runParserIdentity)
+import Circuit.PCA ()
+import Circuit.Poly.Process (systemAsProcess)
+import Circuit.Process (Process, scan)
+import Circuit.LLM.GPT ()
+
+greenLights :: IO ()
+greenLights = do
+  putStrLn "numhask: green"
+  print (one :: Int)
+  putStrLn "numhask-diff: green"
+  print (Proxy :: Proxy (NHD.Diff Double Double))
+  putStrLn "numhask-space: green"
+  print (Point 1 2 :: Point Int)
+  putStrLn "harpie: green"
+  print (array [2, 2] [1, 2, 3, 4] :: Array Int)
+  putStrLn "harpie-numhask: green"
+  putStrLn "process-stats: green"
+  print (scan (ma 0.1) [1, 2, 3 :: Double])
+  putStrLn "circuits: green"
+  print (Proxy :: Proxy (Process Double Double))
+  putStrLn "circuits-poly: green"
+  print (Proxy :: Proxy (Process Double Double))
+  putStrLn "circuits-ad: green"
+  print (Proxy :: Proxy (CAD.Diff Double Double))
+  putStrLn "circuits-mat: green"
+  print (Proxy :: Proxy (Mat Double () ()))
+  putStrLn "circuits-parser: green"
+  print (runParserIdentity (char 'a' :: Parser Identity String Char Char) "abc" :: These Char String)
+  putStrLn "circuits-pca: green"
+  putStrLn "circuits-llm: green"
